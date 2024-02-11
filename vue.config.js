bc011762a50ee91c6f1f90612bc187d20a1f8f07
tzinyongo <51459077+tzinyongo@users.noreply.github.com>
2024-02-11 14:50:56 -0500

    fixed other file routes

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/Deployment/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/Deployment/dist' : '/',
 };
